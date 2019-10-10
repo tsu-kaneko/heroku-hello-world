bd091e70ab5f1c21cf0c8135d7d6a081d017006e
tsu-kaneko <tsu.kaneko0410@gmail.com>
2019-10-10 19:57:11 +0900

    HelloWorld

--- src/main/java/com/example/herokuhelloworld/HerokuHelloWorldApplication.java
+++ src/main/java/com/example/herokuhelloworld/HerokuHelloWorldApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HerokuHelloWorldApplication {
 
+       	@RequestMapping("/")
+	public String home() {
+		return "Hello World in Heroku";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuHelloWorldApplication.class, args);
 	}
