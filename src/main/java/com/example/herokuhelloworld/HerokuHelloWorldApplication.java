6d5408957614f456be4e5235b6b5cb9328a272eb
tsu-kaneko <tsu.kaneko0410@gmail.com>
2019-10-10 19:53:41 +0900

    first commit

---
+++ src/main/java/com/example/herokuhelloworld/HerokuHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokuhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuHelloWorldApplication.class, args);
+	}
+
+}
