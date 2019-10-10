c337b1994f1a3dfd25f684e0a5a73cebe03d58fd
tsu-kaneko <tsu.kaneko0410@gmail.com>
2019-10-10 20:22:48 +0900

    fix controller

--- src/main/java/com/example/herokuhelloworld/HerokuHelloWorldApplication.java
+++ src/main/java/com/example/herokuhelloworld/HerokuHelloWorldApplication.java
@@ -3,13 +3,15 @@ package com.example.herokuhelloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HerokuHelloWorldApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World in Heroku";
+		return "Hello World from Docker";
 	}
 
 	public static void main(String[] args) {
