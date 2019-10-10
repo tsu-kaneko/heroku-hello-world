93c8562432a4a12388694216451bdcc2b89f6427
tsu-kaneko <tsu.kaneko0410@gmail.com>
2019-10-10 20:02:29 +0900

    add import requestmapping

--- src/main/java/com/example/herokuhelloworld/HerokuHelloWorldApplication.java
+++ src/main/java/com/example/herokuhelloworld/HerokuHelloWorldApplication.java
@@ -2,11 +2,12 @@ package com.example.herokuhelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class HerokuHelloWorldApplication {
 
-       	@RequestMapping("/")
+	@RequestMapping("/")
 	public String home() {
 		return "Hello World in Heroku";
 	}
